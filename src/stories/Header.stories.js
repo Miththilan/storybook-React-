efb6c169ec9deb41d4c2cc3555ef04123ed837e9
Miththilan <miththilan3@gmail.com>
2021-11-12 11:15:58 +0530

    Header Story3

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -4,6 +4,12 @@ import React from "react";
 export default {
   title: "Components/Header",
   component: Header,
+  argTypes: {
+    variant: {
+      options: ["h3", "h4", "h5", "h6"],
+      control: { type: "radio" },
+    },
+  },
 };
 
 const Template = (args) => <Header {...args} />;
@@ -11,5 +17,5 @@ const Template = (args) => <Header {...args} />;
 export const Heading = Template.bind({});
 Heading.args = {
   label: "Blog",
-  variant: "h2",
+  //   variant: "h2",
 };
